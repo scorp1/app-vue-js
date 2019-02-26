128352151839b0ab57f5a09a1d23907b1f496dae
Vladimir Beloplotov <cgartscorp@gmail.com>
2019-02-26 22:16:03 +0500

    Backend part - 2 (nodemailer)

--- server/routes/record.js
+++ server/routes/record.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const nodemailer = require('nodemailer');
 const Record = require('../models/Record');
 
 router.get('/', async (req, res) => {
@@ -13,8 +13,43 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
     const record = new Record(req.body);
     await record.save();
+    const output = ` 
+    <p> You have a new message from nice-app:</p>
+    <ul>
+        <li>name: ${req.body.name}</li>
+        <li>email: ${req.body.email}</li>
+        <li>address: ${req.body.address}</li>
+        <li>gender: ${req.body.gender}</li>
+    </ul>
+    `;
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: 'your mailLogin',
+            pass: 'your mailPass'
+        }
+    });
+
+    let mailOptions = {
+        from: 'mailSource your mail',
+        to: 'mailTarget whom send',
+        subject: `nice-course | New message`,
+        text: req.body.name,
+        html: output
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    });
+
     res.json({state: 'success'});
 });
+
 router.put('/:id', async (req, res) => {
     await Record.findByIdAndUpdate(req.params.id, req.body);
     res.json({state: 'updated'})
